d79033ce7a1e3b073a72ca7c625b83242b88be2a
Mario Beltrán Alarcón <belco90@gmail.com>
2018-04-14 20:35:10 +0200

    Remove bootstrap as package dependency

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
