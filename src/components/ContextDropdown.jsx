8d969b634c5b7d5d8d1722980c97f5eb84f8c194
Mario Beltrán Alarcón <belco90@gmail.com>
2018-04-14 21:58:54 +0200

    Handle dropdown open state manually

--- src/components/ContextDropdown.jsx
+++ src/components/ContextDropdown.jsx
@@ -5,9 +5,48 @@ import Dropdown from 'react-bootstrap/es/Dropdown';
 import MenuItem from 'react-bootstrap/es/MenuItem';
 
 class ContextDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false,
+    };
+
+    this.handleDropdownToggle = this.handleDropdownToggle.bind(this);
+    this.handleCancelClick = this.handleCancelClick.bind(this);
+    this.handleApplyClick = this.handleApplyClick.bind(this);
+  }
+
+  handleDropdownToggle(isOpen) {
+    this.setState({ dropdownOpen: isOpen });
+  }
+
+  handleCancelClick(event) {
+    event.preventDefault();
+
+    this.setState({ dropdownOpen: false });
+
+    console.log('TODO: restore filters');
+  }
+
+  handleApplyClick(event) {
+    event.preventDefault();
+
+    this.setState({ dropdownOpen: false });
+
+    console.log('TODO: apply filters');
+  }
+
   render() {
+    const { dropdownOpen } =this.state;
+
     return (
-      <Dropdown id={this.props.id} className="ContextDropdown">
+      <Dropdown
+        id={this.props.id}
+        className="ContextDropdown"
+        open={dropdownOpen}
+        onToggle={this.handleDropdownToggle}
+      >
         <Dropdown.Toggle>
           {this.props.title}
         </Dropdown.Toggle>
@@ -20,8 +59,8 @@ class ContextDropdown extends React.Component {
           <MenuItem divider />
 
           <ButtonToolbar className="ContextDropdown-buttons">
-            <Button bsSize="small" bsStyle="link">Cancel</Button>
-            <Button bsSize="small" bsStyle="default">Apply</Button>
+            <Button bsSize="small" bsStyle="link" onClick={this.handleCancelClick}>Cancel</Button>
+            <Button bsSize="small" bsStyle="default" onClick={this.handleApplyClick}>Apply</Button>
           </ButtonToolbar>
         </Dropdown.Menu>
       </Dropdown>
