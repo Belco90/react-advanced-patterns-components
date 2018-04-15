b87f348f8ae455eb5b70d18ac5635057437e4c6d
Mario Beltrán Alarcón <belco90@gmail.com>
2018-04-16 00:12:26 +0200

    Add second filter

---
+++ src/components/GenderFilter.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ControlLabel from 'react-bootstrap/es/ControlLabel';
+import FormControl from 'react-bootstrap/es/FormControl';
+import FormGroup from 'react-bootstrap/es/FormGroup';
+
+class GenderFilter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+
+    this.handleOptionSelected = this.handleOptionSelected.bind(this);
+  }
+
+  handleOptionSelected(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  applyFilter() {
+    const { value } = this.state;
+
+    console.log(`set value '${value}' in redux store`);
+  }
+
+  restoreFilter() {
+    console.log('restore filter');
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <FormGroup controlId="GenderFilterSelect">
+        <ControlLabel>Content</ControlLabel>
+        <FormControl componentClass="select" value={value}>
+          <option value="" onClick={this.handleOptionSelected}>All</option>
+          <option value="female" onClick={this.handleOptionSelected}>Female</option>
+          <option value="male" onClick={this.handleOptionSelected}>Male</option>
+        </FormControl>
+      </FormGroup>
+    );
+  }
+}
+
+export default GenderFilter;
