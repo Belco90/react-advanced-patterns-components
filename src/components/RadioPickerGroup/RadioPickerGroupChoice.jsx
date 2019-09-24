11588bd800c73fd5bde3e58a96a3b44367992901
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-25 01:53:18 +0200

    Add styles for RadioPickerGroup component

--- src/components/RadioPickerGroup/RadioPickerGroupChoice.jsx
+++ src/components/RadioPickerGroup/RadioPickerGroupChoice.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import RadioGroupChoice from 'components/RadioGroupUsingHooks/RadioGroupChoice';
 import { useRadioGroupContext } from 'contexts/radio-group-context';
+
 import RadioPickerGroupIfChecked from './RadioPickerGroupIfChecked';
+import styles from './RadioPickerGroupChoice.module.css';
 
 const propTypes = {
   value: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  emphasis: PropTypes.oneOf(['default', 'success', 'danger']),
-  hideIndicator: PropTypes.bool,
 };
 
 const defaultProps = {
@@ -22,20 +23,20 @@ const defaultProps = {
 const isIfCheckedComponent = child =>
   React.isValidElement(child) && child.type === RadioPickerGroupIfChecked;
 
-const RadioPickerGroupChoice = ({
-  children,
-  hideIndicator,
-  emphasis,
-  ...remainingProps
-}) => {
-  // TODO: style it properly with hideIndicator and emphasis
+const RadioPickerGroupChoice = ({ children, className, ...remainingProps }) => {
   const context = useRadioGroupContext();
-  const { selectedValue } = context;
+  const { selectedValue, emphasis, hideIndicator } = context;
 
   const checked = remainingProps.value === selectedValue;
 
+  const mergedClassName = cn(className, styles.root, {
+    [styles.checked]: checked,
+    [styles.hideIndicator]: hideIndicator,
+    [styles[emphasis]]: emphasis !== 'default',
+  });
+
   return (
-    <RadioGroupChoice {...remainingProps}>
+    <RadioGroupChoice {...remainingProps} className={mergedClassName}>
       {/* Inject `checked` prop only to IfChecked children */}
       {React.Children.map(children, child =>
         isIfCheckedComponent(child)
--- stories/2-RadioPickerGroup.stories.js
+++ stories/2-RadioPickerGroup.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 import RadioPickerGroup from 'components/RadioPickerGroup';
 
@@ -31,9 +32,36 @@ const makeRadioPickerGroup = (props = {}) => () => {
 export const defaultComponent = makeRadioPickerGroup();
 export const successEmphasis = makeRadioPickerGroup({ emphasis: 'success' });
 export const dangerEmphasis = makeRadioPickerGroup({ emphasis: 'danger' });
-export const hidingRadioIndicator = makeRadioPickerGroup({
-  hideIndicator: true,
-});
+
+export const hidingRadioIndicator = () => {
+  const [value, setValue] = React.useState('a');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <RadioPickerGroup
+      selectedValue={value}
+      name="RadioPickerGroup"
+      onChange={handleChange}
+      hideIndicator
+    >
+      <RadioPickerGroup.Choice value="a">
+        <Image src="https://placeimg.com/50/50/nature" circle />
+        <span style={{ marginLeft: '20px' }}>option A</span>
+      </RadioPickerGroup.Choice>
+      <RadioPickerGroup.Choice value="b">
+        <Image src="https://placeimg.com/50/50/animals" circle />
+        <span style={{ marginLeft: '20px' }}>option B</span>
+      </RadioPickerGroup.Choice>
+      <RadioPickerGroup.Choice value="c">
+        <Image src="https://placeimg.com/50/50/tech" circle />
+        <span style={{ marginLeft: '20px' }}>option C</span>
+      </RadioPickerGroup.Choice>
+    </RadioPickerGroup>
+  );
+};
 
 export const showingConditionalElements = () => {
   const [value, setValue] = React.useState('a');
@@ -49,52 +77,58 @@ export const showingConditionalElements = () => {
       onChange={handleChange}
     >
       <RadioPickerGroup.Choice value="a">
-        option A
+        <strong>option A</strong>
         <RadioPickerGroup.IfChecked>
-          Lorem fistrum amatomaa torpedo ese que llega te voy a borrar el
-          cerito. Hasta luego Lucas pecador está la cosa muy malar llevame al
-          sircoo va usté muy cargadoo quietooor qué dise usteer al ataquerl la
-          caidita papaar papaar a peich. Quietooor quietooor apetecan papaar
-          papaar hasta luego Lucas condemor a gramenawer. La caidita torpedo
-          sexuarl va usté muy cargadoo tiene musho peligro diodenoo. Diodenoo
-          fistro pecador apetecan de la pradera diodenoo. Tiene musho peligro
-          sexuarl llevame al sircoo diodenoo fistro ahorarr. Quietooor qué dise
-          usteer pecador pecador se calle ustée está la cosa muy malar ahorarr
-          hasta luego Lucas me cago en tus muelas. Por la gloria de mi madre
-          diodeno qué dise usteer por la gloria de mi madre ese hombree ese
-          hombree caballo blanco caballo negroorl papaar papaar. No te digo
-          trigo por no llamarte Rodrigor pupita al ataquerl torpedo de la
-          pradera qué dise usteer llevame al sircoo benemeritaar llevame al
-          sircoo pecador.
+          <p>
+            Lorem fistrum amatomaa torpedo ese que llega te voy a borrar el
+            cerito. Hasta luego Lucas pecador está la cosa muy malar llevame al
+            sircoo va usté muy cargadoo quietooor qué dise usteer al ataquerl la
+            caidita papaar papaar a peich. Quietooor quietooor apetecan papaar
+            papaar hasta luego Lucas condemor a gramenawer. La caidita torpedo
+            sexuarl va usté muy cargadoo tiene musho peligro diodenoo. Diodenoo
+            fistro pecador apetecan de la pradera diodenoo. Tiene musho peligro
+            sexuarl llevame al sircoo diodenoo fistro ahorarr. Quietooor qué
+            dise usteer pecador pecador se calle ustée está la cosa muy malar
+            ahorarr hasta luego Lucas me cago en tus muelas. Por la gloria de mi
+            madre diodeno qué dise usteer por la gloria de mi madre ese hombree
+            ese hombree caballo blanco caballo negroorl papaar papaar. No te
+            digo trigo por no llamarte Rodrigor pupita al ataquerl torpedo de la
+            pradera qué dise usteer llevame al sircoo benemeritaar llevame al
+            sircoo pecador.
+          </p>
         </RadioPickerGroup.IfChecked>
       </RadioPickerGroup.Choice>
       <RadioPickerGroup.Choice value="b">
-        option B
+        <strong>option B</strong>
         <RadioPickerGroup.IfChecked>
-          Lorem fistrum a wan ese que llega diodenoo diodeno por la gloria de mi
-          madre. Te voy a borrar el cerito ese que llega ahorarr papaar papaar a
-          peich ahorarr. Por la gloria de mi madre no te digo trigo por no
-          llamarte Rodrigor está la cosa muy malar está la cosa muy malar
-          sexuarl a gramenawer apetecan. Torpedo quietooor va usté muy cargadoo
-          a peich llevame al sircoo la caidita llevame al sircoo te voy a borrar
-          el cerito apetecan. Fistro a peich a gramenawer te voy a borrar el
-          cerito a peich ese pedazo de.
+          <p>
+            Lorem fistrum a wan ese que llega diodenoo diodeno por la gloria de
+            mi madre. Te voy a borrar el cerito ese que llega ahorarr papaar
+            papaar a peich ahorarr. Por la gloria de mi madre no te digo trigo
+            por no llamarte Rodrigor está la cosa muy malar está la cosa muy
+            malar sexuarl a gramenawer apetecan. Torpedo quietooor va usté muy
+            cargadoo a peich llevame al sircoo la caidita llevame al sircoo te
+            voy a borrar el cerito apetecan. Fistro a peich a gramenawer te voy
+            a borrar el cerito a peich ese pedazo de.
+          </p>
         </RadioPickerGroup.IfChecked>
       </RadioPickerGroup.Choice>
       <RadioPickerGroup.Choice value="c">
-        option C
+        <strong>option C</strong>
         <RadioPickerGroup.IfChecked>
-          Lorem fistrum no te digo trigo por no llamarte Rodrigor qué dise
-          usteer me cago en tus muelas. A peich te voy a borrar el cerito la
-          caidita ese hombree no te digo trigo por no llamarte Rodrigor me cago
-          en tus muelas. Va usté muy cargadoo llevame al sircoo te voy a borrar
-          el cerito quietooor. A gramenawer tiene musho peligro está la cosa muy
-          malar torpedo ahorarr. Apetecan jarl te voy a borrar el cerito ahorarr
-          no te digo trigo por no llamarte Rodrigor condemor ese pedazo de la
-          caidita. Mamaar llevame al sircoo va usté muy cargadoo ese pedazo de
-          quietooor. Diodeno pupita diodenoo está la cosa muy malar caballo
-          blanco caballo negroorl apetecan torpedo diodeno hasta luego Lucas
-          está la cosa muy malar a peich.
+          <p>
+            Lorem fistrum no te digo trigo por no llamarte Rodrigor qué dise
+            usteer me cago en tus muelas. A peich te voy a borrar el cerito la
+            caidita ese hombree no te digo trigo por no llamarte Rodrigor me
+            cago en tus muelas. Va usté muy cargadoo llevame al sircoo te voy a
+            borrar el cerito quietooor. A gramenawer tiene musho peligro está la
+            cosa muy malar torpedo ahorarr. Apetecan jarl te voy a borrar el
+            cerito ahorarr no te digo trigo por no llamarte Rodrigor condemor
+            ese pedazo de la caidita. Mamaar llevame al sircoo va usté muy
+            cargadoo ese pedazo de quietooor. Diodeno pupita diodenoo está la
+            cosa muy malar caballo blanco caballo negroorl apetecan torpedo
+            diodeno hasta luego Lucas está la cosa muy malar a peich.
+          </p>
         </RadioPickerGroup.IfChecked>
       </RadioPickerGroup.Choice>
     </RadioPickerGroup>
