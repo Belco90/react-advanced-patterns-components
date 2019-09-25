085ff29d119ec7750646bf2d0ca8d3ddd96da799
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-25 23:23:47 +0200

    Remove old stuff

--- stories/App.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from 'components/App';
-import configureStore from 'configureStore';
-import { Provider } from 'react-redux';
-
-import 'index.css';
-
-export default { title: 'App' };
-export const defaultApp = () => <App />;
